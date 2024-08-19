76dfcde0a9253250725a42dc52103e10941844c0
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-08-19 20:34:11 +0530

    toc

--- src/components/TOClink.tsx
+++ src/components/TOClink.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import clsx from 'clsx';
 import * as React from 'react';
 
@@ -23,13 +25,15 @@ export default function TOCLink({
       href={`#${id}`}
       id={`link-${id}`}
       className={clsx(
-        'font-medium hover:text-gray-700 focus:outline-none dark:hover:text-gray-200',
-        'focus-visible:text-gray-700 dark:focus-visible:text-gray-200',
+        'block py-1 px-2 rounded-md transition-colors duration-200',
+        'font-medium',
+        'focus:outline-none focus-visible:ring-2 ring-blue-500',
         activeSection === id
-          ? 'text-gray-900 dark:text-gray-100'
-          : 'text-gray-400 dark:text-gray-500'
+          ? 'text-blue-600 dark:text-blue-400 bg-blue-50 dark:bg-blue-800'
+          : 'text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700'
       )}
-      style={{ marginLeft: (level - minLevel) * 16 }}
+      style={{ marginLeft: Math.max((level - minLevel) * 16, 0) }}
+      scroll
     >
       {text}
     </UnstyledLink>
